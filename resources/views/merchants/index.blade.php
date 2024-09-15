1ff642b5215bd7c1732a87634dd3cead7b206ea4
Rico Hermawan Situmorang <104193392+Cho18@users.noreply.github.com>
2024-09-15 22:48:40 +0700

    second commit

--- resources/views/merchants/index.blade.php
+++ resources/views/merchants/index.blade.php
@@ -1,26 +1,29 @@
 @extends('dashboard.index')
 
 @section('content')
-<div class="container">
-    <h1>Daftar Katering</h1>
-
-    <!-- Search form -->
-    <form action="{{ route('merchants.index') }}" method="GET">
-        <div class="input-group mb-3">
-            <input type="text" name="search" class="form-control" placeholder="Cari berdasarkan Company Name, Address, Contact, Description" value="{{ request()->input('search') }}">
-            <button class="btn btn-primary" type="submit">Search</button>
+<div class="container mt-5">
+    <h1 class="text-center">Daftar Katering</h1>
+    <div class="row mb-3">
+        <div class="col-md-6 offset-md-3">
+            <form action="{{ route('merchants.index') }}" method="GET">
+                <div class="input-group">
+                    <input type="text" name="search" class="form-control" placeholder="Cari katering" value="{{ request()->input('search') }}">
+                    <button class="btn btn-primary" type="submit">
+                        <i class="fas fa-search"></i> Cari
+                    </button>
+                </div>
+            </form>
         </div>
-    </form>
+    </div>
 
-    <!-- Merchants table -->
     <table id="merchantsTable" class="table table-bordered">
         <thead>
             <tr>
-                <th>Company Name</th>
-                <th>Address</th>
-                <th>Contact</th>
-                <th>Description</th>
-                <th>Action</th>
+                <th>Nama Katering</th>
+                <th>Alamat</th>
+                <th>Kontak</th>
+                <th>Deskripsi</th>
+                <th>Aksi</th>
             </tr>
         </thead>
         <tbody>
@@ -37,21 +40,5 @@
             @endforeach
         </tbody>
     </table>
-
-    <!-- Pagination links -->
-    {{ $merchants->withQueryString()->links() }}
-
 </div>
-@endsection
-
-@section('scripts')
-<!-- Initialize DataTables -->
-<script>
-    $(document).ready(function() {
-        $('#merchantsTable').DataTable({
-            "paging": false, // Disable pagination in DataTables since we're using Laravel pagination
-            "searching": false // Disable built-in search to use Laravel's search
-        });
-    });
-</script>
 @endsection
