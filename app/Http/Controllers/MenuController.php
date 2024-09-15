92493f0fb948bd5e323de87c91fe9eb49bdbe102
Rico Hermawan Situmorang <104193392+Cho18@users.noreply.github.com>
2024-09-15 22:38:00 +0700

    second commit

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Menu;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class MenuController extends Controller
 {
@@ -13,25 +14,20 @@ class MenuController extends Controller
      */
     public function index(Request $request)
     {
-        // Ambil role_id dan user_id dari pengguna yang sedang login
         $userRoleId = Auth::user()->role_id;
         $userId = Auth::id();
 
-        // Jika role_id adalah 2
         if ($userRoleId == 2) {
-            // Filter menu berdasarkan user_id
-            $menus = Menu::where('user_id', $userId)->get();
+            $menus = Menu::where('user_id', $userId)->paginate(12);
         } else {
-            // Untuk role_id=1
-            // Cek jika ada query pencarian
             $search = $request->get('search');
             $menus = Menu::when($search, function ($query, $search) {
                 return $query->where('name', 'like', "%{$search}%")
-                             ->orWhere('description', 'like', "%{$search}%")
-                             ->orWhere('price', 'like', "%{$search}%");
-            })->get();
+                            ->orWhere('description', 'like', "%{$search}%")
+                            ->orWhere('price', 'like', "%{$search}%");
+            })->paginate(12);
         }
-
+        
         return view('menu.index', compact('menus'));
     }
 
@@ -48,29 +44,36 @@ class MenuController extends Controller
      */
     public function store(Request $request)
     {
-        // Validasi input
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'description' => 'required|string',
-            'price' => 'required|numeric',
-            'photo' => 'required|image|max:2048',
-            'type_of_food' => 'required|in:nasi goreng,ayam goreng,mie goreng,lontong,nasi ayam', // New validation
+        $validated = $request->validate([
+            'name'              => 'required|string|max:255',
+            'description'       => 'required|string',
+            'price'             => 'required|numeric',
+            'photo'             => 'required|image|mimes:jpg,jpeg,png|max:2048',
+            'type_of_food'      => 'required|in:nasi goreng,ayam goreng,mie goreng,lontong,nasi ayam',
+        ], [
+            'name.required'              => 'Nama menu makanan wajib diisi',
+            'description.required'       => 'Deskripsi menu makanan wajib diisi',
+            'price.required'             => 'Harga menu makanan wajib diisi',
+            'photo.required'             => 'Foto menu makanan wajib diisi',
+            'photo.image'                => 'Harus berupa gambar',
+            'photo.mimes'                => 'Hanya file JPG, JPEG, PNG yang diizinkan',
+            'photo.max'                  => 'Ukuran gambar maksimal 2MB',
+            'type_of_food.required'      => 'Jenis menu makanan wajib diisi',
+            'type_of_food.in'            => 'Jenis menu makanan tidak valid',
         ]);
 
-        // Handle file upload untuk photo
         $filePath = $request->file('photo')->store('menu_photos', 'public');
 
-        // Buat menu baru
         Menu::create([
-            'name' => $request->name,
-            'description' => $request->description,
-            'price' => $request->price,
-            'photo' => $filePath,
-            'user_id' => auth()->id(),
-            'type_of_food' => $request->type_of_food, // Store type of food
+            'name'              => $request->name,
+            'description'       => $request->description,
+            'price'             => $request->price,
+            'photo'             => $filePath,
+            'user_id'           => auth()->id(),
+            'type_of_food'      => $request->type_of_food, 
         ]);
 
-        return redirect()->route('menu.index')->with('success', 'Menu berhasil dibuat.');
+        return redirect()->route('menu.index')->with('success', 'Menu makanan berhasil dibuat.');
     }
 
     /**
@@ -88,35 +91,49 @@ class MenuController extends Controller
     {
         // Validasi input
         $request->validate([
-            'name' => 'required|string|max:255',
-            'description' => 'required|string',
-            'price' => 'required|numeric',
-            'photo' => 'nullable|image|max:2048',
-            'type_of_food' => 'required|in:nasi goreng,ayam goreng,mie goreng,lontong,nasi ayam', // New validation
+            'name'              => 'required|string|max:255',
+            'description'       => 'required|string',
+            'price'             => 'required|numeric',
+            'photo'             => 'nullable|image|mimes:jpg,jpeg,png|max:2048',
+            'type_of_food'      => 'required|in:nasi goreng,ayam goreng,mie goreng,lontong,nasi ayam',
+        ], [
+            'name.required'              => 'Nama menu makanan wajib diisi.',
+            'name.max'                   => 'Nama menu makanan tidak boleh lebih dari 255 karakter.',
+            'description.required'       => 'Deskripsi menu makanan wajib diisi.',
+            'price.required'             => 'Harga menu makanan wajib diisi.',
+            'price.numeric'              => 'Harga menu makanan harus berupa angka.',
+            'photo.image'                => 'Foto harus berupa gambar.',
+            'photo.mimes'                => 'Hanya file dengan tipe jpg, jpeg, png yang diizinkan.',
+            'photo.max'                  => 'Ukuran gambar maksimal 2MB.',
+            'type_of_food.required'      => 'Jenis menu makanan makanan wajib diisi.',
+            'type_of_food.in'            => 'Jenis menu makanan makanan tidak valid.',
         ]);
 
-        // Update photo jika diunggah
         if ($request->hasFile('photo')) {
+            if ($menu->photo) {
+                Storage::disk('public')->delete($menu->photo);
+            }
+
             $filePath = $request->file('photo')->store('menu_photos', 'public');
             $menu->photo = $filePath;
         }
 
-        // Update kolom lainnya
         $menu->update([
-            'name' => $request->name,
-            'description' => $request->description,
-            'price' => $request->price,
-            'type_of_food' => $request->type_of_food, // Update type of food
+            'name'              => $request->name,
+            'description'       => $request->description,
+            'price'             => $request->price,
+            'type_of_food'      => $request->type_of_food,
         ]);
 
-        return redirect()->route('menu.index')->with('success', 'Menu item updated successfully.');
+        return redirect()->route('menu.index')->with('success', 'Menu makanan berhasil diperbarui.');
     }
+
     /**
      * Remove the specified menu item from storage.
      */
     public function destroy(Menu $menu)
     {
         $menu->delete();
-        return redirect()->route('menu.index')->with('success', 'Menu item deleted successfully.');
+        return redirect()->route('menu.index')->with('success', 'Menu makanan berhasil dihapus');
     }
 }
